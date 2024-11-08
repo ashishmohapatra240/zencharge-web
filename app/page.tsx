4740321bb3f7d64ab912465498756e87621a17a2
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2024-11-08 09:49:37 +0530

    Initialized Project

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import FifthSection from "@/components/FifthSection";
+import FourthSection from "@/components/FourthSection";
+import HeroSection from "@/components/HeroSection";
+import SecondSection from "@/components/SecondSection";
+import SixthSection from "@/components/SixthSection";
+import ThirdSection from "@/components/ThirdSection";
+
+export default function Home() {
+  return (
+    <div>
+      <HeroSection />
+      <SecondSection />
+      <ThirdSection />
+      <FourthSection />
+      <FifthSection />
+      <SixthSection />
+    </div>
+  );
+}
