81d08cb8beb51736dd643a51ad88a2284672dc05
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2024-12-05 10:09:06 +0530

    Add Blog Data

---
+++ app/lib/constants/blog-data.ts
@@ -0,0 +1,21 @@
+export const blogPosts = [
+    {
+        id: 1,
+        title: "ZenCharge Café Models: Redefining Travel and Dining with Sustainability",
+        date: "November 27, 2024",
+        author: "ZenCharge Editorial Team",
+        image: "/images/about/cafe-models.png",
+        excerpt: "As electric vehicles (EVs) become increasingly popular, the need for sustainable, convenient, and comfortable roadside amenities is more critical than ever...",
+        content: `Introduction:
+    As electric vehicles (EVs) become increasingly popular, the need for sustainable, convenient, and comfortable roadside amenities is more critical than ever. ZenCharge is revolutionizing the way we think about travel and dining...`
+    },
+    {
+        id: 2,
+        title: "The Rise of Sustainable Travel: Why FeVL Cafés Are the Future",
+        date: "November 27, 2024",
+        author: "ZenCharge Editorial Team",
+        image: "/images/about/fevl-future.png",
+        excerpt: "Discover how FeVL cafés are transforming the landscape of sustainable travel...",
+    },
+    // ... Add more blog posts
+]; 
