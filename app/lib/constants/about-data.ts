6f60f8c7b12899d1c9c6b884d8cf1c41d61a0f76
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2024-12-05 10:08:59 +0530

    Add About Data

---
+++ app/lib/constants/about-data.ts
@@ -0,0 +1,18 @@
+export const aboutStats = [
+    {
+        title: '5000 charging stations',
+        description: 'With over 1,000 charging stations across India, we are committed to making EV charging convenient and accessible for all users.'
+    },
+    {
+        title: '200 partnerships',
+        description: 'We have established 50 strategic partnerships with local businesses to enhance the EV charging experience.'
+    },
+    {
+        title: '1000 users served',
+        description: 'To date, we have successfully served over 200 users, ensuring a seamless charging experience for every electric vehicle owner.'
+    },
+    {
+        title: '50 cities',
+        description: 'Our network spans across 10 major cities, with plans to expand to 50 cities in the next year, making sustainable mobility a reality for more people.'
+    }
+] 
