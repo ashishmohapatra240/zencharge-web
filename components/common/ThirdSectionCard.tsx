27623408daa5d5eae4812489b3f164f0a0deca91
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2024-12-12 12:36:52 +0530

    Refactor ThirdSectionCard component: enhance layout with responsive design, replace <img> with <Image> for optimized loading, and improve text styling for better readability.

--- components/common/ThirdSectionCard.tsx
+++ components/common/ThirdSectionCard.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 
 interface CardProps {
   title: string;
@@ -8,23 +9,18 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ title, description, imageSrc }) => {
   return (
-    <div className="w-full max-w-[400px] flex flex-col items-start gap-4 p-4 rounded-lg bg-white">
-      <div className="w-full h-[250px] mb-4">
-        <img
-          src={imageSrc}
-          alt={title}
-          className="w-full h-full object-cover rounded-lg"
-        />
-      </div>
-      <h2 className="text-[#1E1E1E] text-xl font-semibold font-montserrat">
+    <div className="flex-1 min-w-[280px] max-w-[450px] flex flex-col gap-6 p-6 bg-white rounded-lg shadow-sm">
+      <Image
+        src={imageSrc}
+        alt={title}
+        className="w-full aspect-video object-cover rounded-lg"
+      />
+      <h3 className="text-xl md:text-2xl font-bold font-montserrat text-[#1E1E1E]">
         {title}
-      </h2>
-      <p className="text-[#1E1E1E] text-sm font-normal leading-relaxed font-montserrat">
+      </h3>
+      <p className="text-base font-normal font-montserrat text-[#1E1E1E] leading-relaxed">
         {description}
       </p>
-      <button className="mt-4 px-5 py-2 text-base font-medium text-[#1E1E1E] bg-white border border-gray-200 rounded-md hover:bg-gray-100">
-        Learn More
-      </button>
     </div>
   );
 };
